434a21f187b1ecc98789ccea42ad647807211142
Josh-Gotro <joshuagauthreaux@gmail.com>
2020-07-11 01:58:55 -0500

    contact section blocked out and styled

--- src/SayHi.js
+++ src/SayHi.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import './customCSS/Hi.css'
 
 const SayHi = () => {
     return (
+        <div className="HiContainer">
         <div id="hello">
-            sayhi
+            <h1>say hello</h1>
+        </div>
+        <div className="HiForm ">
+        <form>
+            <label>
+                <input id="name" name="name" type="text" placeholder="name"></input><br></br>
+                <input id="email" name="email" type="text" placeholder="email"></input><br></br>
+                <textarea id="message" name="message" placeholder="enter message" ></textarea><br></br>
+                <button id="hiButton" type="submit" value="Submit" >send</button>
+            </label>
+        </form>
+        </div>
         </div>
     );
 }
