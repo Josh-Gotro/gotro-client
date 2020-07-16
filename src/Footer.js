5abf4d92502c89ee753788bc8fb4c584d6a28412
Josh-Gotro <joshuagauthreaux@gmail.com>
2020-07-16 01:42:51 -0500

    svgr added, social icons imported, components created, footer created

--- src/Footer.js
+++ src/Footer.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import GitIcon from './Icons/Github.js';
+import LinkedIcon from './Icons/Linkedin.js'
+import StravaIcon from './Icons/Strava.js'
+
+import './customCSS/Footer.css';
 
 const Footer = () => {
+
+    
     return (
-        <div>
-            <i className="fab fa-github-alt"></i>
+        <div className="Footer">
+            
+            <a href="https://github.com/Josh-Gotro"><GitIcon width="40px" height="40px" /></a>
+            
+            <a href="https://www.linkedin.com/in/josh-gauthreaux/"><LinkedIcon width="40px" height="40px" /></a>
 
-
+            <a href="https://www.strava.com/athletes/25087475"><StravaIcon width="40px" height="40px" /></a>
         </div>
     );
 }
