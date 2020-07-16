5b15ac2195f94edf59a6aebbb27123b3b9f0d94d
Josh-Gotro <joshuagauthreaux@gmail.com>
2020-07-16 02:01:04 -0500

    social icon plates styled

--- src/Footer.js
+++ src/Footer.js
@@ -6,16 +6,12 @@ import StravaIcon from './Icons/Strava.js'
 import './customCSS/Footer.css';
 
 const Footer = () => {
-
     
     return (
         <div className="Footer">
-            
-            <a href="https://github.com/Josh-Gotro"><GitIcon width="40px" height="40px" /></a>
-            
-            <a href="https://www.linkedin.com/in/josh-gauthreaux/"><LinkedIcon width="40px" height="40px" /></a>
-
-            <a href="https://www.strava.com/athletes/25087475"><StravaIcon width="40px" height="40px" /></a>
+            <div className="Social sOne"><a href="https://github.com/Josh-Gotro"><GitIcon width="24px" height="24px" /></a></div>
+            <div className="Social sTwo"><a href="https://www.linkedin.com/in/josh-gauthreaux/"><LinkedIcon width="24px" height="24px" /></a></div>
+            <div className="Social sThree"><a href="https://www.strava.com/athletes/25087475"><StravaIcon width="24px" height="24px" /></a></div>
         </div>
     );
 }
