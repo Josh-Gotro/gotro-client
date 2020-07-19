c505a546e63276d818267c45bfbce8be3469a823
Josh-Gotro <joshuagauthreaux@gmail.com>
2020-07-19 09:27:27 -0500

    links open to new tab

--- src/Footer.js
+++ src/Footer.js
@@ -9,9 +9,9 @@ const Footer = () => {
     
     return (
         <div className="Footer">
-            <div className="Social sOne"><a href="https://github.com/Josh-Gotro"><GitIcon width="24px" height="24px" /></a></div>
-            <div className="Social sTwo"><a href="https://www.linkedin.com/in/josh-gauthreaux/"><LinkedIcon width="24px" height="24px" /></a></div>
-            <div className="Social sThree"><a href="https://www.strava.com/athletes/25087475"><StravaIcon width="24px" height="24px" /></a></div>
+            <div className="Social sOne"><a href="https://github.com/Josh-Gotro" target="_blank" ><GitIcon width="24px" height="24px" /></a></div>
+            <div className="Social sTwo"><a href="https://www.linkedin.com/in/josh-gauthreaux/" target="_blank"><LinkedIcon width="24px" height="24px" /></a></div>
+            <div className="Social sThree"><a href="https://www.strava.com/athletes/25087475" target="_blank"><StravaIcon width="24px" height="24px" /></a></div>
         </div>
     );
 }
