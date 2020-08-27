8a03650e072f236c47cc59d4ee91687d8a5e7e85
Josh-Gotro <joshuagauthreaux@gmail.com>
2020-08-27 15:43:28 -0500

    blog titles &amp; logic fixed, add margins and borders around articles

--- src/Blog.js
+++ src/Blog.js
@@ -7,32 +7,32 @@ const Blog = () => {
     const mediumUrl = "https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fmedium.com%2Ffeed%2F%40joshuagauthreaux";
     const [myBlog, setMyBlog] = useState([]);
     const abortController = new AbortController();
-    
-    useEffect(() =>{
-        fetch(mediumUrl)
-        .then(res => res.json())
-        .then( data => {
-            setMyBlog(data)
-        return function cleanup() {
-            abortController.abort()
-        }
-        })
-    },[mediumUrl]);
 
-    function displayBlogs() {
-        if(myBlog.items){ 
-            return myBlog.items.map(blog => {
-                if(blog.categories.length > 0){
-                    // console.log(blog)
-                    return <BlogTile key={blog.pubDate} blogData={blog} /> 
+    useEffect(() => {
+        fetch(mediumUrl)
+            .then(res => res.json())
+            .then(data => {
+                setMyBlog(data)
+                return function cleanup() {
+                    abortController.abort()
                 }
             })
-        }
+    }, [mediumUrl]);
+
+    function displayBlogs() {
+            return myBlog.items && myBlog.items.map(blog => {
+                if (blog.categories.length > 0) {
+                    return <BlogTile key={blog.pubDate} blogData={blog} />
+                }
+            })
     }
-        
+
     return (
         <>
-           {displayBlogs()} 
+            {/* <h1>B<br />L<br />O<br />G</h1> */}
+            <div className="Blogs">
+                {displayBlogs()}
+            </div>
         </>
     );
 }
--- src/BlogTile.js
+++ src/BlogTile.js
@@ -1,15 +1,52 @@
 import React from 'react';
+import './customCSS/Blog.css';
+
 
 const BlogTile = (props) => {
+    const { title, link, thumbnail, content, pubDate } = props.blogData;
 
-    function doSomething(){
-        console.log(props.blogData)
-        return props.blogData.title
+    function cleanTitle(checkTitle) {
+        checkTitle = checkTitle.replace("amp;", "");
+        return checkTitle
     }
 
+    function truncateText(text, start, len){
+        return text.length > len ? text.slice(start, len) : text;
+    }
+
+    function toText(node){
+        let tag = document.createElement('div');
+        tag.innerHTML = node;
+        node = tag.innerText;
+        return node
+    }
+
+    function convertDate(date){
+        let dateArray = date.slice(0, 10).split("-")
+        let year = dateArray.shift();
+        dateArray.push(year)
+        return `Published: ${dateArray.join("/")}`;  
+    }
+
+    function blogPost(){
+        return      <a href={`${link}`}>
+                    <div className="ImageContainer">
+                        <img src={`${thumbnail}`} className="Image"></img>
+                    </div>
+                    <div className="TDContainer">
+                        {/* <h3>{cleanTitle(title)}</h3> */}
+                        <h3>{truncateText(cleanTitle(title), 0, 40) + "..."}</h3><br/>
+                        <p>{truncateText(toText(content), 60, 300) + "..."}</p><br />
+                        <h4>{convertDate(pubDate)}</h4>
+                    </div>
+                    </a> 
+                
+    }
+
+
     return (
-        <div>
-            {doSomething()}
+        <div className="BlogContainer">
+            {blogPost()}
         </div>
     );
 }
