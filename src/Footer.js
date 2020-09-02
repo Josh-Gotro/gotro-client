411c140d0981ff1d12117580044c8e8244da251b
Josh-Gotro <joshuagauthreaux@gmail.com>
2020-09-02 10:57:48 -0500

    add link to CV

--- src/Footer.js
+++ src/Footer.js
@@ -3,6 +3,8 @@ import GitIcon from './Icons/Github.js';
 import LinkedIcon from './Icons/Linkedin.js'
 import StravaIcon from './Icons/Strava.js'
 import './customCSS/Footer.css';
+import resume from './images/Josh S Gauthreaux Resume.pdf'
+
 
 const Footer = () => {
 
@@ -11,6 +13,7 @@ const Footer = () => {
             <div className="Social sOne"><a href="https://github.com/Josh-Gotro" target="_blank" ><GitIcon width="24px" height="24px" /></a></div>
             <div className="Social sTwo"><a href="https://www.linkedin.com/in/josh-gauthreaux/" target="_blank"><LinkedIcon width="24px" height="24px" /></a></div>
             <div className="Social sThree"><a href="https://www.strava.com/athletes/25087475" target="_blank"><StravaIcon width="24px" height="24px" /></a></div>
+            <div ><a href="https://learn.co/munificent-macro-8796/resume" target="_blank">My CV</a></div>
         </div>
     );
 }
