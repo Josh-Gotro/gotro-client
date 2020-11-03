710d1719eca21263b238fc0fc526067c4051d3b6
Josh-Gotro <joshuagauthreaux@gmail.com>
2020-11-03 16:29:22 -0600

    add border below RW to soften transition to blog

--- src/RecentWork.js
+++ src/RecentWork.js
@@ -77,6 +77,7 @@ const RecentWork = () => {
                 <div className="Full">{viewProject}</div>
             </div>
         </div>
+        <div className="zigzag"></div>
     </>
     );
 }
